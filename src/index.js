cc16032700f761dbd8fa0ac175af3facd81b38d7
kim <kimngan.ledang@zattoo.com>
2024-06-19 21:57:12 +0200

    implement game

--- src/index.js
+++ src/index.js
@@ -1,26 +1,218 @@
-// Test import of a JavaScript module
-import { example } from '@/js/example'
+import '@/styles/index.scss';
 
-// Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+import { UI } from '@/js/UI';
+import { Prediction } from './js/Prediction';
 
-// Test import of styles
-import '@/styles/index.scss'
+import camConfig from '@/js/CameraConfig';
 
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+// store a reference to the player video
+let playerVideo;
 
-const heading = document.createElement('h1')
-heading.textContent = example()
+// keep track of scores
+let playerScore = 0, computerScore = 0;
 
-// Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
 
-// Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
+/**
+ * Set up & initialization
+ */
+const onInit = async () => {
 
-const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+  UI.init();
+  UI.setStatusMessage("Initializing - Please wait a moment");
+
+  const videoPromise = UI.initPlayerVideo(camConfig);
+  const predictPromise = Prediction.init();
+
+  Promise.all([videoPromise, predictPromise])
+    .then(result => {
+
+      // result[0] will contain the initialized video element
+      playerVideo = result[0];
+      playerVideo.play();
+
+      // game is ready
+      waitForPlayer();
+    });
+}
+
+/**
+ * Game logic
+ */
+const waitForPlayer = () => {
+
+  // show a blinking start message
+  if(UI.isMobile()) {
+    UI.setStatusMessage("Touch the screen to start");
+  } else {
+    UI.setStatusMessage("Press any key to start");
+  }
+
+  UI.startAnimateMessage();
+
+  const startGame = () => {
+    UI.stopAnimateMessage();
+    playOneRound();
+  };
+
+  // wait for player to press any key
+  // then stop blinking and play one round
+  if(UI.isMobile()) {
+    document.addEventListener('touchstart', startGame, { once: true });
+  } else {
+    window.addEventListener("keypress", startGame, { once: true });
+  }
+}
+
+const playOneRound = async () => {
+
+  // show robot waiting for player
+  UI.showRobotImage(true);
+
+  // hide the timer circle
+  UI.showTimer(false);
+  UI.setTimerProgress(0);
+  UI.setPlayerHand("");
+
+  // ready - set - show
+  // wait for countdown to finish
+  await UI.startCountdown();
+
+  // show the timer circle
+  UI.showTimer(true);
+
+  // start detecting player gestures
+  // required duration 150ms ~ 4-5 camera frames
+  detectPlayerGesture(150);
+}
+
+const detectPlayerGesture = (requiredDuration) => {
+
+  let lastGesture = "";
+  let gestureDuration = 0;
+
+  const predictNonblocking = () => {
+
+    setTimeout(() => {
+
+      const predictionStartTS = Date.now();
+
+      // predict gesture (require high confidence)
+      Prediction.predictGesture(playerVideo, 9).then(playerGesture => {
+
+        if(playerGesture !== "") {
+
+          if(playerGesture === lastGesture) {
+            // player keeps holding the same gesture
+            // -> keep timer running
+            const deltaTime = Date.now() - predictionStartTS;
+            gestureDuration += deltaTime;
+          }
+          else {
+            // detected a different gesture
+            // -> reset timer
+            UI.setPlayerHand(playerGesture);
+            lastGesture = playerGesture;
+            gestureDuration = 0;
+          }
+        }
+        else {
+          UI.setPlayerHand(false);
+          lastGesture = "";
+          gestureDuration = 0;
+        }
+
+        if(gestureDuration < requiredDuration) {
+          // update timer and repeat
+          UI.setTimerProgress(gestureDuration / requiredDuration);
+          predictNonblocking();
+        }
+        else {
+
+          // player result available
+          // -> stop timer and check winner
+          UI.setTimerProgress(1);
+          UI.animatePlayerHand();
+
+          // let computer make its move
+          const computerGesture = getRandomGesture();
+
+          // check the game result
+          checkResult(playerGesture, computerGesture);
+        }
+      });
+
+    }, 0);
+  };
+
+  predictNonblocking();
+}
+
+const checkResult = (playerGesture, computerGesture) => {
+
+  let statusText;
+  let playerWins = false;
+  let computerWins = false;
+
+  if(playerGesture == computerGesture) {
+    // draw
+    statusText = "It's a draw!";
+  }
+  else {
+    // check whinner
+    if(playerGesture === "rock") {
+      if(computerGesture === "scissors") {
+        playerWins = true;
+        statusText = "Rock beats scissors";
+      } else {
+        computerWins = true;
+        statusText = "Paper beats rock";
+      }
+    }
+    else if(playerGesture === "paper") {
+      if(computerGesture === "rock") {
+        playerWins = true;
+        statusText = "Paper beats rock";
+      } else {
+        computerWins = true;
+        statusText = "Scissors beat paper";
+      }
+    }
+    else if(playerGesture === "scissors") {
+      if(computerGesture === "paper") {
+        playerWins = true;
+        statusText = "Scissors beat paper";
+      } else {
+        computerWins = true;
+        statusText = "Rock beats scissors";
+      }
+    }
+  }
+
+  if(playerWins) {
+    playerScore++;
+    statusText += " - You win!";
+  }
+  else if(computerWins) {
+    computerScore++;
+    statusText += " - The robot wins!";
+  }
+
+  UI.showRobotHand(true);
+  UI.setRobotGesture(computerGesture);
+
+  UI.setStatusMessage(statusText);
+
+  UI.setPlayerScore(playerScore);
+  UI.setRobotScore(computerScore);
+
+  // wait for 3 seconds, then start next round
+  setTimeout(playOneRound, 3000);
+}
+
+const getRandomGesture = () => {
+  const gestures = ["rock", "paper", "scissors"];
+  const randomNum = Math.floor(Math.random() * gestures.length);
+  return gestures[randomNum];
+}
+
+onInit();
---
+++ src/js/CameraConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    audio: false,
+    video: {
+        facingMode: "user",
+        width: 640,
+        height: 640,
+        frameRate: { max: 30 }
+    }
+};
+
+export default config;
